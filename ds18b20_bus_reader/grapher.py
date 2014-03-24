3ba9dfff2a01f5fe0253ac976caf6809b3d4e4ce
Jean-Paul Calderone <exarkun@twistedmatrix.com>
2014-03-24 09:43:08 -0400

    a post-processor to make something I can gnuplot

---
+++ ds18b20_bus_reader/grapher.py
@@ -0,0 +1,18 @@
+from sys import stdin
+from datetime import datetime
+
+def main():
+    for line in stdin:
+        if b"No more addresses." in line:
+            continue
+        pieces = line.split()
+        timestamp = datetime.strptime(pieces.pop(0), b"%Y-%m-%dT%H:%M:%S.%f")
+        mapping = dict(piece.split(b"=", 1) for piece in pieces)
+        print timestamp.isoformat(), mapping[b"ADDRESS"], mapping[b"TEMP"][:-3]
+
+main()
+
+# set format x "%H:%M"
+# set timefmt "%Y-%m-%dT%H:%M:%S"
+# set xdata time
+# plot [ ] [0:25] '/tmp/sensor-a.txt' using 1:3, '/tmp/sensor-b.txt' using 1:3
